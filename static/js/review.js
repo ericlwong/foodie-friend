0974336a625faf56b8a714e0cdcff87836205681
Eric Wong <imericlwong@gmail.com>
2023-06-05 09:23:04 -0700

    Fix adding event listener only if button exists

--- static/js/review.js
+++ static/js/review.js
@@ -1,16 +1,18 @@
-const deleteReviewBtn = document.querySelector('#delete-review-btn');
+const deleteReviewBtn = document.querySelector('#delete-review-btn') || null;
 
-deleteReviewBtn.addEventListener('click', () => {
-  fetch('/api/delete-review', {
-    method: 'POST',
-    body: JSON.stringify({ userReviewId: deleteReviewBtn.value, }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((response) => response.json())
-    .then((responseJson) => {
-      deleteReviewBtn.parentElement.remove();
-      alert(responseJson.status);
-    });
-});
+if (deleteReviewBtn !== null) {
+  deleteReviewBtn.addEventListener('click', () => {
+    fetch('/api/delete-review', {
+      method: 'POST',
+      body: JSON.stringify({ userReviewId: deleteReviewBtn.value, }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        deleteReviewBtn.parentElement.remove();
+        alert(responseJson.status);
+      });
+  });
+}
