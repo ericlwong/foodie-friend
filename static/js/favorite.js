7b32f43c23673e21cd5ca55ef367971e51433ffe
Eric Wong <imericlwong@gmail.com>
2023-06-05 09:26:02 -0700

    Fix adding event listener only if fav btn exists

--- static/js/favorite.js
+++ static/js/favorite.js
@@ -1,22 +1,24 @@
-const saveButton = document.querySelector('#save');
+const saveButton = document.querySelector('#save') || null;
 
-saveButton.addEventListener('click', (evt) => {
-  evt.preventDefault();
-
-  const data = {
-    restaurantId: saveButton.value,
-  };
-
-  fetch(`/api/save`, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((response) => response.json())
-    .then((responseJson) => {
-      alert(responseJson.status);
-      bootstrap.Modal.getInstance(document.querySelector('#favorites-modal')).hide();
-    });
-});
+if (saveButton !== null) {
+  saveButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+  
+    const data = {
+      restaurantId: saveButton.value,
+    };
+  
+    fetch(`/api/save`, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        alert(responseJson.status);
+        bootstrap.Modal.getInstance(document.querySelector('#favorites-modal')).hide();
+      });
+  });
+}
